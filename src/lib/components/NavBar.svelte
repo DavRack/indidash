7465dd40ae7c7a34813c3cb0f7f1665ff8fb8080
david_londono <David.Londono@nordstrom.com>
2024-02-25 18:53:00 -0500

    update

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+<div class="
+  w-full flex flex-row justify-end items-end
+  space-x-4 px-16 py-2 bg-blue-400 font-bold text-white
+  ">
+  <a href="/" class="mr-auto text-3xl">IndiDash</a>
+  <a href="/">Blog</a>
+  <a href="/">Ingresar</a>
+</div>
